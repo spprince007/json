028fc381a1d5feb6c3214e4ba1cc7fb4580ef2cd
Spprince007 <spprince007@gmail.com>
2022-08-30 15:38:35 +0600

    appand chile

--- js/fetch.js
+++ js/fetch.js
@@ -3,23 +3,46 @@ function loadData() {
         .then(response => response.json())
         .then(data => displayUser(data))
 }
-
 function displayUser(data) {
-    // for (const user of data) {
-    //     console.log(user.name, user.email);
-
-    // }
-
     const ul = document.getElementById('user-list');
     for (const user of data) {
         const li = document.createElement('li');
         li.innerText = user.name;
         ul.appendChild(li);
     }
-    for (const user of data) {
-        const li = document.createElement('li');
-        li.innerText = user.email;
-        ul.appendChild(li);
-    }
 }
+
+
+// function displayUser(data) {
+//     const ul = document.getElementById('user-list');
+//     for (const user of data) {
+//         const li = document.createElement('li');
+//         li.innerText = user.name;
+//         ul.appendChild(li);
+
+//     }
+
+// }
+
+
+
+
+// function displayUser(data) {
+//     // for (const user of data) {
+//     //     console.log(user.name, user.email);
+
+//     // }
+
+//     const ul = document.getElementById('user-list');
+//     for (const user of data) {
+//         const li = document.createElement('li');
+//         li.innerText = user.name;
+//         ul.appendChild(li);
+//     }
+//     for (const user of data) {
+//         const li = document.createElement('li');
+//         li.innerText = user.email;
+//         ul.appendChild(li);
+//     }
+// }
 
